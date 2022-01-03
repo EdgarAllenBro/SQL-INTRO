13897cfe2e2a7b8e4f60bf81bcffb571aba76374
Cameron Grieder <cjgrieder@gmail.com>
2022-01-03 14:07:11 -0700

    artist section done

---
+++ artist.sql
@@ -0,0 +1,7 @@
+INSERT INTO artist(name)
+VALUE ('chuck'),('timmy')('billip')
+
+SELECT * FROM artist DESC LIMIT 10;
+SELECT * FROM artist ASC limit 5;
+SELECT * FROM artist WHERE name LIKE 'black%';
+SELECT * FROM artist WHERE name LIKE '%black%';
