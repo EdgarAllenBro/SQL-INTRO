87d1ca56beb3949db60d1f03a0a9f6f5e0c3d71a
Cameron Grieder <cjgrieder@gmail.com>
2022-01-03 12:46:16 -0700

    finished problems relating to persons.sql

--- person.sql
+++ person.sql
@@ -7,7 +7,7 @@ CREATE TABLE person(
   fav_color VARCHAR(20));
 
 INSERT INTO person (name,age,height,city,fav_color)
-VALUES('DANNY',23,173,'murrieta','blue'), 
+VALUES('DANNY',13,159,'murrieta','blue'), 
 ('cameron', 26,179,'springville','purple'),
 ('tristan',28,182,'canada','yellow'),
 ('rene',56,185,'springville','red'),
@@ -18,3 +18,33 @@ ORDER BY height DESC;
 
 SELECT * FROM person
 ORDER BY height;
+
+SELECT * FROM person
+ORDER BY age DESC;
+
+SELECT * FROM person 
+WHERE age > 20;
+
+SELECT * FROM person
+WHERE age = 18;
+
+SELECT * FROM person
+WHERE age <20 OR age>30;
+
+SELECT * FROM person 
+WHERE age <> 27;
+
+SELECT * FROM person
+WHERE fav_color <> 'red';
+
+SELECT * FROM person
+WHERE fav_color <> 'red' and fav_color <> 'blue';
+
+SELECT * FROM person
+WHERE fav_color = 'orange' OR fav_color = 'green';
+
+SELECT * FROM person
+WHERE fav_color IN('orange','green','blue');
+
+SELECT * FROM person
+WHERE fav_color = 'yellow' OR 'purple';
